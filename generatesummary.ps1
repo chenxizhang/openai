2d90a574a6844cdd769c26d55004e7c773cc6940
chenxizhang <ares@xizhang.com>
2023-12-17 09:52:14 +0800

    udpate

--- generatesummary.ps1
+++ generatesummary.ps1
@@ -24,7 +24,10 @@ function Get-MDSummary {
 
 
 $content = Get-ChildItem docs\chapter*.md | `
-    Select-Object -Property Name, @{Name = 'index'; Expression = { [convert]::ToInt16( (Select-String -Pattern '\d+' -InputObject $_.Name).Matches.value) } } | `
+    Select-Object -Property Name,`
+     @{Name = 'index'; `
+     Expression = { [convert]::ToInt16( `
+        (Select-String -Pattern '\d+' -InputObject $_.Name).Matches.value) } } | `
     Sort-Object -Property index |`
     ForEach-Object {
     Get-MDSummary -file "docs\$($_.Name)" 
